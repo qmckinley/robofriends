38152190a9fe29ba827c2a763c8d5ca166a97e3f
Q.McKinley <q.mckinley@outlook.com>
2019-04-13 14:47:17 -0400

    optimized build

---
+++ build/precache-manifest.5036a01f98e7adb1a6a6da3bbde18eb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3675dd8438d6283c654a",
+    "url": "/static/js/main.40e54f79.chunk.js"
+  },
+  {
+    "revision": "5bdd9f7c68ed796609af",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "3675dd8438d6283c654a",
+    "url": "/static/css/main.4c6c2a60.chunk.css"
+  },
+  {
+    "revision": "5bdd9f7c68ed796609af",
+    "url": "/static/css/2.d177c93b.chunk.css"
+  },
+  {
+    "revision": "1f5b89620287cd9de2d24848169fccc1",
+    "url": "/index.html"
+  }
+];
